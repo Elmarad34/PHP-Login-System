73af17bd966b396e86eb7884e8d8414bfbefb46c
elmarad2011 <elmarad2011@gmail.com>
2018-12-11 12:33:50 +0200

    Added index

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
+<? php 
+	//allow the config
+	define ('__CONFIG__'. true);
+	//require the config
+	require_once "inc/config.php"; ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<!-- UIkit CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/css/uikit.min.css" />
 	<title></title>
+</head>
 		<body>
-	<!-- UIkit JS -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.25/js/uikit-icons.min.js"></script>
-		<div class="uk-section uk-container"></div>
-		<div class="uk-grid uk-chikd-width-1-3-@s uk-child-with-1"></div>
-		<form class="uk-form-stacked js-login">
-
-	    <div class="uk-margin">
-	        <label class="uk-form-label" for="form-stacked-text">Email</label>
-	        <div class="uk-form-controls">
-	            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-	        </div>
-	    </div
-
-	    <div class="uk-margin">
-	        <label class="uk-form-label" for="form-stacked-text">Password</label>
-	        <div class="uk-form-controls">
-	            <input class="uk-input" id="form-stacked-text" type="Password " required = "required" placeholder="Your Password">
-	        </div>
-	    </div>
-
-	    <div class="uk-margin">
-	    	<button class="uk-button uk.button-default" type="submit">LOGIN</button>
+		<div class="uk-section uk-container">
+			<?php
+				echo "Hello World. Today is : ";
+				echo date("Y md d");
+			?>
+		</div>
 	    </div>	    
 
 </form>
+	<? php requre_once "inc/footer.php"; ?>
 	</body>
-</head>
 </html>
